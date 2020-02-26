10775933ce51c4da60036546af57266cce5066b9
Anu-Bhav <myster.anubhav@gmail.com>
2020-02-26 03:53:21 +0000

    Templates

--- flaskblog.py
+++ flaskblog.py
@@ -1,18 +1,41 @@
-from flask import Flask, escape, request
+from flask import Flask, escape, request, render_template, url_for
 
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'John Doe',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'Febuary 20, 2020',
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'Febuary 21, 2020',
+    },
+    {
+        'author': 'Sam Smith',
+        'title': 'Blog Post 3',
+        'content': 'Third post content',
+        'date_posted': 'Febuary 22, 2020',
+    },
+]
 
 
 @app.route('/')
 @app.route('/home')
+# def home():
+#     name = request.args.get("name", "Anubhav")
+#     return f'<h1>Hello, {escape(name)}!</h1>'
 def home():
-    name = request.args.get("name", "Anubhav")
-    return f'<h1>Hello, {escape(name)}!</h1>'
+    return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
 def about():
-    return f'<h1>About Page</h1>'
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
