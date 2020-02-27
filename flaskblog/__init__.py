356965c5fe5919762add15d6105ea4f9c9d834a1
Anu-Bhav <myster.anubhav@gmail.com>
2020-02-27 20:29:17 +0000

    Email and Password Reset

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -2,16 +2,25 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8aa2e62480a55d23b269ba558d10bb4d'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 # the 3 /// are a relative path from the current file
 
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+
+app.config['MAIL_SERVER'] = 'mail.local'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USERNAME'] = 'app@mail.local'
+app.config['MAIL_PASSWORD'] = 'Password'
+mail = Mail(app)
 
 from flaskblog import routes
